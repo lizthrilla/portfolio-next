8ac5c8a2c843d5979f6f03455ee08c3c340c18a5
Liz Tiller <elizabeth.tiller@gmail.com>
2025-04-09 12:31:55 -0400

    new download page

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -11,7 +11,7 @@ import {
     {name: "Github", href: "https://github.com/lizthrilla", icon: <FaGithub />},
     {name: "eMail", href: "mailto:elizabeth.tiller@gmail.com?Subject=Hello%20again", icon: <FaEnvelope />},
     {name: "Twitter", href: "https://twitter.com/liztiller", icon: <FaTwitter />},
-    {name: "Resume", href: "https://docs.google.com/document/d/1FF4tocHbjnaTjyb8wSMrnPlDk6XHfcAxAmOHYXyJ3uY/pub", icon: <FaDownload /> },
+    {name: "Resume", href: "https://docs.google.com/document/d/e/2PACX-1vRinEQg4HOxBH0_ZwMaAgJjd4Ur_yPFDtYQRabnEKJo-5p3mo0Kfx5nyRzPVF96Ag/pub ", icon: <FaDownload /> },
   ]
 
   const SocialLinks = () => (
