dbccbbbabe241c655d75565855df61d75812a009
Liz Tiller <elizabeth.tiller@gmail.com>
2025-04-09 12:43:10 -0400

    removed twitter

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -10,7 +10,7 @@ import {
     {name: "LinkedIn", href: "https://www.linkedin.com/in/elizabethtiller/", icon: <FaLinkedin />},
     {name: "Github", href: "https://github.com/lizthrilla", icon: <FaGithub />},
     {name: "eMail", href: "mailto:elizabeth.tiller@gmail.com?Subject=Hello%20again", icon: <FaEnvelope />},
-    {name: "Twitter", href: "https://twitter.com/liztiller", icon: <FaTwitter />},
+    // {name: "Twitter", href: "https://twitter.com/liztiller", icon: <FaTwitter />},
     {name: "Resume", href: "https://docs.google.com/document/d/e/2PACX-1vRinEQg4HOxBH0_ZwMaAgJjd4Ur_yPFDtYQRabnEKJo-5p3mo0Kfx5nyRzPVF96Ag/pub ", icon: <FaDownload /> },
   ]
 
