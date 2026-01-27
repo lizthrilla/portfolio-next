cf929f9ebcc8258fdb1340ba740e9c5644d4892c
Liz Tiller <elizabeth.tiller@gmail.com>
2026-01-27 16:35:31 -0500

    case study clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Navbar, SkipLink } from "./components/index";
 import "./globals.css";
-// import "./main.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
